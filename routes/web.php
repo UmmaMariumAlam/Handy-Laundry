44e6b0cb27b9e2fe82d943dc6624242a0eb2378d
ridawarida <Nahin.warida@gmail.com>
2025-05-15 23:21:58 +0600

    removed old '/' route

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('LaundromatHome');
-});
-
-
-Route::get('/', function () {
     return view('welcome');
 });
 
